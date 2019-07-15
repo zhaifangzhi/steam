5dbfe43152a10132d565170c49c2f1b7d2b6e2c8
zhaifangzhi <zhaifangyuan2@gmail.com>
2019-07-15 10:49:17 +0800

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -64,7 +64,7 @@ html body {
 	<!-- I wouldn't touch this part -->
 	
 		<form action="index.php" method="post" style="margin-bottom:0;">
-			<input name="url" type="text" style="width:400px;" autocomplete="off" placeholder="http://" />
+			<input name="url" type="text" style="width:400px;" autocomplete="on" placeholder="http://" />
 			<input type="submit" value="Go" />
 		</form>
 		
